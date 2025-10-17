d8a700dd03edbcc8f1f0c45da0b78678779f047a
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-10-17 17:57:06 +0900

    check_mos.pyのCSV読み込みを更新し、NaNを除いた有効スコア数と平均値を計算するロジックを追加

--- tools/check_mos.py
+++ tools/check_mos.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv(
-    "data_tabidachi/moshi_stage3_new_jchat_tabidachi/NISQA_results.csv"
-)
-mean_mos = df["mos_pred"].mean()
-print("Average MOS:", mean_mos)
+# CSV読み込み
+df = pd.read_csv("data_sample_audio_nisqa/tabidachi/NISQA_results.csv")
+
+# NaNを除いた有効スコア数と平均値を計算
+valid_mos = df["mos_pred"].dropna()
+count_mos = len(valid_mos)
+mean_mos = valid_mos.mean()
+
+print(f"Average MOS: {mean_mos:.2f}")
+print(f"Number of MOS values: {count_mos}")
